f22c443ea2fae31354ea3ef0b03fc79bce7c7a29
Faisal Ahmed Sifat <Faisalahmed531@gmail.com>
2022-10-31 14:00:57 +0600

    added routes to get user photo, and information

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,14 @@ const cors = require('cors');
 
 const app = express();
 
+const config = require("./utils/config");
+// const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+
 const { sequelize } = require('./models');
 
 const mainRoutes = require('./routes/mainRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const connectDB = async () => {
   try {
@@ -25,13 +30,16 @@ connectDB();
 //  *
 //  * CORS, Express, ExpressJSON
 //  */
-// app.use(cors());
-// app.use(express.static("build"));
-// app.use(express.json());
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
 
 app.use('/', mainRoutes)
+app.use('/user', userRoutes)
 // app.get('/', (req,res)=>{
 //     res.send('hello world')
 // })
+
+// app.use(middleware.unknownEndpoint);
 
 module.exports = app;
