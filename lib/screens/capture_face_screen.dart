627ac7f4642a558cccd893bf0bfb9c11bb898538
hasibullah1811 <hasib.ascend@gmail.com>
2022-11-01 17:00:56 +0600

    camera service initialized

---
+++ lib/screens/capture_face_screen.dart
@@ -0,0 +1,77 @@
+import 'package:amazing_app/custom_widgets/custom_button_large.dart';
+import 'package:amazing_app/services/camera_service.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CaptureFaceScreen extends StatefulWidget {
+  static const String routeName = "CaptureFaceScreen";
+  final List<CameraDescription> cameras;
+
+  const CaptureFaceScreen({super.key, required this.cameras});
+
+  @override
+  State<CaptureFaceScreen> createState() => _CaptureFaceScreenState();
+}
+
+class _CaptureFaceScreenState extends State<CaptureFaceScreen> {
+  late CameraController cameraController;
+  late Future<void> cameraValue;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startCamera();
+  }
+
+  void startCamera() async {
+    cameraController = CameraController(
+        widget.cameras[1], ResolutionPreset.high,
+        enableAudio: false);
+    cameraValue = cameraController.initialize();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    cameraController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          FutureBuilder(
+              future: cameraValue,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Container(
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width,
+                      child: CameraPreview(cameraController));
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }),
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: CustomButtonLarge(title: "Capture"),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColorLight = Color(0xFF0008C1);
+const Color primaryColorDark = Color(0xFF6C4AB6);
+const Color primaryIconColorDark = Color(0xFF16c79a);
+
+final Color primaryTextColorDarkHE = const Color(0xFFFFFFFF).withOpacity(0.87);
+final Color primaryTextColorDarkME = const Color(0xFFFFFFFF).withOpacity(0.60);
+final Color primaryTextColorDarkDE = const Color(0xFFFFFFFF).withOpacity(0.37);
