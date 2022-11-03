cfea7d2097f492d9b22770ac612980e086193e78
Faisal Ahmed Sifat <Faisalahmed531@gmail.com>
2022-11-03 10:48:31 +0600

    added google authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:amazing_app/screens/login_screen.dart';
+
 import 'services/services.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -23,7 +25,8 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const HomeScreen(),
+        // home: const HomeScreen(),
+        home: const LoginScreen(),
         routes: {
           HomeScreen.routeName: ((context) => const HomeScreen()),
         },
